9c8c0539cf7f07ae5f7b9e0553e3dfe74b500b25
Cameron Allan <camallan16@gmail.com>
2025-03-20 20:41:59 -0400

    css changes and added an experience page

---
+++ src/components/Experience.jsx
@@ -0,0 +1,15 @@
+import '../assets/experience.css';
+import Footer from './Footer';
+import NavBar from './NavBar';
+
+function Experience() {
+  return (
+    <div className="experience">
+      <NavBar />
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default Experience;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,26 +5,35 @@ import '../assets/navbar.css';
 function NavBar() {
   return (
     <div className="nav-bar">
-      <p className="title">Cameron Allan</p>
-      <nav>
-        <ul>
-          <li>
-            <NavLink className="link" to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="link" to="/About">
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="link" to="/Projects">
-              Projects
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className="nav-content">
+        <p className="title">Cameron Allan</p>
+      </div>
+      <div className="nav-content">
+        <nav>
+          <ul>
+            <li>
+              <NavLink className="link" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to="/About">
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to="/Projects">
+                Projects
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to="/Experience">
+                Experience
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
