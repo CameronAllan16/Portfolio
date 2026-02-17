2a31ba30224e036a54cb153a00477bf4298d8074
Cameron Allan <camallan16@gmail.com>
2026-02-17 13:19:23 -0500

    extracted data from inline to separate file

---
+++ src/assets/data/projectCards.jsx
@@ -0,0 +1,22 @@
+import Img1 from '../data/pic-of-me.jpg';
+
+const projectCards = [
+  {
+    title: 'Estate Vault',
+    shortDescription:
+      'EstateVault is an all-in-on estate management system where estate owners can store and manage their important estate documents.',
+    longDescription:
+      'EstateVault is an all-in-on estate management system where estate owners can store and manage their important estate documents, such as wills, power of attorney, but also a place to store external account information like Facebook login information. The system also allows for users to upload and share estate documents with approved connections with the user.',
+    image: Img1
+  },
+  {
+    title: 'Portfolio',
+    shortDescription:
+      'This portfolio website built using React to showcase my projects and skills.',
+    longDescription:
+      'This portfolio website built using React to showcase my projects and to showcase my skills as a sandbox.',
+    image: Img1
+  }
+];
+
+export default projectCards;
